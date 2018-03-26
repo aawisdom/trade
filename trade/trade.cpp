dac62cc6ea20696a19fae3fbcc5bc634b9b5f6e2
aawisdom <mcgrath_ch@hotmail.com>
2018-03-25 23:05:48 -0400

    Rough sample program with price tests. All codes will change soon. This is just a test initial program.

--- trade/trade.cpp
+++ trade/trade.cpp
@@ -3,8 +3,18 @@ using namespace std;
 
 void main() {
 
-	cout << "hello world." << endl;//test2
+	double current_price, sell_at_price, previous_price, buy_at_price;
 
+	cout << "Checking current price..." << endl; 
+	cin >> current_price;// this will come from brokerage API interface
+	cout << "current price is: " << current_price <<endl;
+	previous_price = current_price;
+	cout << "Checking current price..." << endl;
+	cin >> current_price;
+	if (current_price<previous_price){ cout << "Price dropping..." << endl; }
+	if (current_price>previous_price) { cout << "Price climbing..." << endl; }
+	if (current_price==previous_price) { cout << "Price staying the same..." << endl; }
+	/* program will continue from here. */
 
 	system("pause");
 
